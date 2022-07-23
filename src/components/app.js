b5ace0f94b0a9b878bb1fc865e58253c1f2f12dc
Samuel Banya <sbanya@fastmail.com>
2022-07-23 19:59:36 -0400

    Adding App component to project, and additional notes

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./About.js";
+import Upload from "./Upload.js";
+import Board from "./Board.js";
+
+function App() {
+  return (
+    <div>
+      <About />
+      <Upload />
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+// This will contain the data that will store images for the user in the temporary session
