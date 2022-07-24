eb0e6a49cdf7875426e47162062848e7fce46362
Samuel Banya <sbanya@fastmail.com>
2022-07-23 20:15:12 -0400

    Adding related starter components for project

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This web app allows a user to upload drawing image reference photos so
+        that they can use them to create art!
+      </p>
+      <h2></h2>
+      <p>Functions that the user should be able to do include: </p>
+      <ul>
+        <li>Upload art reference images</li>
+        <li>Rotate images</li>
+        <li>Favorite images</li>
+        <li>Tag images</li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
