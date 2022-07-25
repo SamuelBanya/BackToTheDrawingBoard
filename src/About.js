588251b28ff194e6761588fabc9c7d45f17e6b4b
Samuel Banya <sbanya@fastmail.com>
2022-07-25 18:39:14 -0400

    Adding initial GitHub Pages boilerplate code

---
+++ src/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <h2>About Page</h2>
+    </div>
+  );
+};
+export default About;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Link to="/about">About</Link>
+      <h2>Github Pages</h2>
+      <h3>Deploying React to Github</h3>
+    </div>
+  );
+}
+export default App;
