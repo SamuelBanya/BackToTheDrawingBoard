7ce23bdd980fc1283c1ae0a126ae421688ad485b
Samuel Banya <sbanya@fastmail.com>
2022-07-25 18:28:25 -0400

    Isolating app so that we can deploy it on GitHub Pages first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// This will contain the data that will store images for the user in the temporary session
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
