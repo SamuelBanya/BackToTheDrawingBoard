74b4144fd1172d3d6d6de3d9867079c78ec58e5b
Samuel Banya <sbanya@fastmail.com>
2022-07-27 14:28:00 -0400

    Re-arranging routes to see if that helps with the routing issue present

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./About";
 
+// Try adding 'exact' tag to the route itself
+// Try re-deploying it with '/about' in front
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
