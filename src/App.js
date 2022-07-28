347ebe8527e5f59454d35608075c6a412db4e0a9
Samuel Banya <sbanya@fastmail.com>
2022-07-28 18:54:41 -0400

    Switching back to HashRouter to make 404 errors go away for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import NavBar from "./NavBar.js";
+import NavBar from "./NavBar";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./About";
+import Upload from "./Upload";
+import Board from "./Board";
+
+//       <HashRouter>
+//         <Route exact path="/about" component={About} />
+//         <Route exact path="/upload" component={Upload} />
+//         <Route exact path="/board" component={Board} />
+//       </HashRouter>
 
 function App() {
   return (
