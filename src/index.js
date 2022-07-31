87e5df10b5ce689da6ead283e026c56d1c719534
Samuel Banya <sbanya@fastmail.com>
2022-07-31 11:56:42 -0400

    Removing previous comment

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 
-// Try adding 'exact' tag to the route itself
-// Try re-deploying it with '/about' in front
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
