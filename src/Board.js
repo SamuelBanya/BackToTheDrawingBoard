f34072e3a0ea8b609611a8326f3ef7e0099b1679
Samuel Banya <sbanya@fastmail.com>
2022-08-05 16:59:12 -0400

    Adding 'Draggable' component to project

--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import Draggable from "react-draggable";
 
 function Board() {
   const [photos, setPhotos] = useState([]);
@@ -13,6 +15,11 @@ function Board() {
 
   console.log("photos: ", photos);
 
+  // photos.map((photo) => {
+  // console.log("Using .map() to go through each 'photo' from 'photos': ");
+  // console.log("photo: ", photo);
+  // });
+
   return (
     <div>
       <h2>Board</h2>
@@ -23,6 +30,23 @@ function Board() {
       <select id="tagSelect" name="">
         Tag
       </select>
+      {photos.map((photo) => {
+        console.log("Using .map() to go through each 'photo' from 'photos': ");
+        console.log("photo: ", photo);
+      })}
+      <Draggable
+        axis="x"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+      >
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable>
     </div>
   );
 }
