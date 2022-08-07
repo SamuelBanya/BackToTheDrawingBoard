3d81788761a1e6066d4b4275b777ae2be9e1f97f
Samuel Banya <sbanya@fastmail.com>
2022-08-07 15:19:21 -0400

    Including .map() method to iterate through 'photos' array

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,4 @@
-import React from "react";
-// import { render } from "react-dom";
-// import { createRoot } from "react-dom/client";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 import { Resizable } from "re-resizable";
@@ -9,33 +7,55 @@ import Draggable from "react-draggable";
 const imageUrl =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7Ki-ys2G_MMb_xCrY7nAf87F5ZiIOyCh4f5H_JCTTtMSMLCL";
 
-// NOTE:
-// Because this example uses 'Class', I can't use 'useState', and actually have to opt for the 'this.setState()' function
-// This is so that I can set each Class instance's state individually:
-// https://reactjs.org/docs/hooks-state.html
-
-// NOTE:
-// I will also have to use this section of the React docs as a reference page for this idea
-// so that I can use a 'Class' with 'useEffect()':
-// https://reactjs.org/docs/hooks-effect.html
-
-// export default class Board extends React.Component {
-//   render = () => (
 function Board() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/photos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPhotos(data);
+      });
+  }, []);
+
+  console.log("photos after useEffect() call: ", photos);
+
+  console.log("typeof(photos): ", typeof photos);
+
   return (
     <Draggable>
-      <Resizable
-        defaultSize={{
-          width: 200,
-          height: 360,
-        }}
-        style={{
-          background: `url(${imageUrl})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-        }}
-        lockAspectRatio={true}
-      ></Resizable>
+      <div>
+        {photos.map((photo) => {
+          console.log("photo: ", photo);
+          // console.log(
+          // 'photo["photoArray"]["photoName"]: ',
+          // photo["photoArray"]["photoName"]
+          // );
+          return (
+            <Resizable
+              // 'photos' is an array of objects
+              // Each 'photo' object contains a 'dataArray' array that also contains an object
+              // TODO: Figure out how to actually determine the 'key' value since its so nested:
+              // key={photos["dataArray"][photo]["photoName"]}
+              defaultSize={{
+                width: 200,
+                height: 360,
+              }}
+              style={{
+                background: `url(${imageUrl})`,
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+              }}
+              lockAspectRatio={true}
+            ></Resizable>
+          );
+        })}
+      </div>
     </Draggable>
   );
 }
