61cd606d501f56326a732dc19585bb2ea6553c84
Samuel Banya <sbanya@fastmail.com>
2022-08-09 21:05:21 -0400

    Added 'Back To The Future' font to 'AppHeader' CSS class

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Photo from "./Photo.js";
-import "./style.css";
+import "./index.css";
 
 function Board() {
   const [photos, setPhotos] = useState([]);
@@ -22,22 +22,30 @@ function Board() {
 
   return (
     <div>
-      {photos.forEach((photo) => {
-        console.log(
-          'photo["dataArray"][0]["photoName"]: ',
-          photo["dataArray"][0]["photoName"]
-        );
+      {photos ? (
+        photos.forEach((photo) => {
+          console.log(
+            'photo["dataArray"][0]["photoName"]: ',
+            photo["dataArray"][0]["photoName"]
+          );
 
-        console.log(
-          'photo["dataArray"][0]["photoLink"]: ',
-          photo["dataArray"][0]["photoLink"]
-        );
+          console.log(
+            'photo["dataArray"][0]["photoLink"]: ',
+            photo["dataArray"][0]["photoLink"]
+          );
 
+          <Photo
+            photoName={photo["dataArray"][0]["photoName"]}
+            photoLink={photo["dataArray"][0]["photoLink"]}
+          />;
+        })
+      ) : (
         <Photo
-          photoName={photo["dataArray"][0]["photoName"]}
-          photoLink={photo["dataArray"][0]["photoLink"]}
-        />;
-      })}
+          photoName="Girl With The Pearl Earring"
+          photoLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7Ki-ys2G_MMb_xCrY7nAf87F5ZiIOyCh4f5H_JCTTtMSMLCL"
+        />
+      )}
+      <h2 className="Tagbar">Tags Present:</h2>
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./index.css";
 
 const linkStyles = {
   display: "inline-block",
-  width: "50px",
+  width: "60px",
   padding: "12px",
   margin: "0 6px 6px",
-  background: "green",
+  background: "#00fc8f",
   textDecoration: "none",
-  color: "white",
+  fontWeight: "bold",
+  color: "black",
 };
 
 // <NavLink
@@ -21,8 +23,9 @@ const linkStyles = {
 
 function NavBar() {
   return (
-    <div className="navbar">
-      <h1>Back To The Drawing Board: An Image Reference Board Web App!</h1>
+    <div className="Navbar">
+      <h1 className="AppHeader">Back & Drawing Board:</h1>
+      <h2>An Image Reference Board Web App!</h2>
       <NavLink
         to="/"
         style={linkStyles}
