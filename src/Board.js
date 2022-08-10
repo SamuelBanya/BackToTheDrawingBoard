deee8370a1161df7777e782e0fc223147c07bc43
Samuel Banya <sbanya@fastmail.com>
2022-08-10 14:27:35 -0400

    Had help from Ryan to fix 'Photo' component based return statement in Board component

--- src/Board.js
+++ src/Board.js
@@ -18,36 +18,17 @@ function Board() {
       });
   }, []);
 
-  // TODO: Figure out why the 'Photo' component is actually created
+  const photoToDisplay = photos.map((photo) => {
+    return (
+      <Photo
+        key={photo["dataArray"].key}
+        photoName={photo["dataArray"][0]["photoName"]}
+        photoLink={photo["dataArray"][0]["photoLink"]}
+      />
+    );
+  });
 
-  return (
-    <div>
-      {photos ? (
-        photos.forEach((photo) => {
-          console.log(
-            'photo["dataArray"][0]["photoName"]: ',
-            photo["dataArray"][0]["photoName"]
-          );
-
-          console.log(
-            'photo["dataArray"][0]["photoLink"]: ',
-            photo["dataArray"][0]["photoLink"]
-          );
-
-          <Photo
-            photoName={photo["dataArray"][0]["photoName"]}
-            photoLink={photo["dataArray"][0]["photoLink"]}
-          />;
-        })
-      ) : (
-        <Photo
-          photoName="Girl With The Pearl Earring"
-          photoLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7Ki-ys2G_MMb_xCrY7nAf87F5ZiIOyCh4f5H_JCTTtMSMLCL"
-        />
-      )}
-      <h2 className="Tagbar">Tags Present:</h2>
-    </div>
-  );
+  return <div>{photoToDisplay}</div>;
 }
 
 export default Board;
